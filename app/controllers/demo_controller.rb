46e36a0c1836579c37d54d0cea4178db7e06d142
hooker <mahooker.16@gmail.com>
2014-04-07 01:33:34 -0400

    Fixed tab in demo_controller.rb

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -9,7 +9,7 @@ class DemoController < ApplicationController
   	#render('index')
   	@array = [1,2,3,4,5]
   	@id = params[:id].to_i
-	@page = params[:page].to_i
+		@page = params[:page].to_i
   end
   
   def other_hello
